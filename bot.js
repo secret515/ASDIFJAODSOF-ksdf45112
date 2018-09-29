659f1c9ca7ee2214b5835dced7e8086238d4cd1b
secret515 <41595419+secret515@users.noreply.github.com>
2018-09-29 19:19:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
+
 
 client.on('message', message => {
     const swearWords = ["heck", "damn","darn"];
@@ -9,6 +12,3 @@ client.on('message', message => {
         message.author.send('Hey! That word has been banned, please don\'t use it!');
       }
 })
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
