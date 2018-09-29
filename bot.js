852833ef58665eda6880f531891f5e45b35e9936
secret515 <41595419+secret515@users.noreply.github.com>
2018-09-29 19:20:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,9 @@ const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
 
 
-client.on('message', message => {
-    const swearWords = ["heck", "damn","darn"];
-    if( swearWords.some(word => message.content.includes(word)) ) {
-        message.delete();
-        message.author.send('Hey! That word has been banned, please don\'t use it!');
-      }
-})
+        var filterWords = ["fool", "dumb", "couch potato"];
+        var rgx = new RegExp(filterWords.join(""), "gi");
+
+        function wordFilter(str) {
+        	return str.replace(rgx, "****");
+        }
